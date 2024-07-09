f95b971b117befa606377d177781c0eead2ef718
Timur15V <volkovtimur042@gmail.com>
2024-07-10 00:30:40 +0300

    Сделал парсер для сайта autoopt.ru, требует доп теста.

---
+++ parser.py
@@ -0,0 +1,6 @@
+import requests
+
+articul = "236Д-1000149"
+
+link = f"https://www.autoopt.ru/search/index?search={articul}"
+
---
+++ parsingSites/autoopt_parser.py
@@ -0,0 +1,113 @@
+import json
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+article = "50-1601215"
+
+url = f"https://www.autoopt.ru/search/index?search={article}"
+
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+}
+
+req = requests.get(url, headers=headers)
+
+src = req.text
+
+result_mas = []
+
+soup = BeautifulSoup(src, "lxml")
+
+all_products = soup.find_all(
+    "div", class_="n-catalog-item relative grid-item n-catalog-item__product"
+)
+
+for product in all_products:
+
+    # получение кода товара
+    product_code = product.find(
+        "div", class_="n-catalog-item__photo-code"
+    ).find(
+        "span", class_="string bold n-catalog-item__click-copy n-catalog-item__ellipsis"
+    ).text
+
+    # получение названия бренда товара
+    product_brand = product.find(
+        "div", class_="n-catalog-item__brand d-none d-md-table-cell"
+    ).find(
+        class_="n-catalog-item__ellipsis"
+    ).text.strip()
+
+    # получение артикула товара
+    product_article = product.find(
+        "div", class_="n-catalog-item__article"
+    ).find(
+        "span", class_="n-catalog-item__ellipsis"
+    ).text
+
+    # получение названия товара
+    product_name = product.find(
+        "div", class_="string"
+    ).find(
+        "a", class_="n-catalog-item__name-link"
+    ).text.strip()
+
+    # получение цен товара
+    if product.find("div", class_="n-catalog-item__price-box col-12 col-md pr-0 mb-2") == None:
+        pass
+    else:
+
+        product_prices = product.find(
+            "div", class_="n-catalog-item__price-box col-12 col-md pr-0 mb-2"
+        ).find(
+            "ul"
+        ).find_all(
+            "li"
+        )
+
+        product_price_obj = {}
+
+        for price in product_prices:
+            type_price = price.find(
+                class_="fake mr-2 link-color price5").text.strip()
+            cur_price = price.find(class_="gray").text
+            product_price_obj[type_price] = cur_price
+
+    # получение количества товаров
+    count = product.find(
+        "span", class_="fake grass bold mr-0"
+    )
+
+    if count != None:
+        product_count = product.find(
+            "div", class_="n-catalog-item__count-box"
+        ).find(
+            "span", class_="fake grass bold mr-0"
+        ).text.strip()[:-1]
+        product_count = int(product_count)
+    else:
+        count = product.find(
+            "span", class_="fake link-gray"
+        )
+        if count != None:
+            product_count = count.text
+        else:
+            product_count = product.find(
+                "offers"
+            ).get(":offers")
+            product_count = json.loads(product_count)[0]["quantity"]
+
+    result_mas.append(
+        {
+            "Код товара": product_code,
+            "Бренд товара": product_brand,
+            "Артикул товара": product_article,
+            "Цены товара": product_price_obj,
+            "Количество на складе": product_count,
+        }
+    )
+
+with open("data.json", "w", encoding="utf-8") as file:
+    json.dump(result_mas, file, ensure_ascii=False)
