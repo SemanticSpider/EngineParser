ab1a1ae0d6e4bfb480b117008df685dafb8aa1de
Timur <volkovtimur042@gmail.com>
2024-08-09 18:56:50 +0300

    Сделал парсер для автоопта

--- parsingSites/autoopt_parser.py
+++ parsingSites/autoopt_parser.py
@@ -1,9 +1,21 @@
 import json
 import sys
 import requests
+from time import sleep
+from random import uniform
 from bs4 import BeautifulSoup
 
-article = "ТС107-1306100-06М24"
+articles = ["322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010", 
+            "322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010",
+			"322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010",
+            "322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010",
+            "322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010",
+            "322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010",
+            "322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010",
+            "322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010",
+            "322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010"]
+
+article = "3302-1602408"
 
 url = f"https://www.autoopt.ru/search/index?search={article}"
 
@@ -12,102 +24,127 @@ headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
 }
 
-req = requests.get(url, headers=headers)
+def get_product_info(article=article):
 
-src = req.text
+    url = f"https://www.autoopt.ru/search/index?search={article}"
 
-result_mas = []
+    req = requests.get(url, headers=headers)
 
-soup = BeautifulSoup(src, "lxml")
+    src = req.text
 
-all_products = soup.find_all(
-    "div", class_="n-catalog-item relative grid-item n-catalog-item__product"
-)
+    result_mas = []
 
-for product in all_products:
+    soup = BeautifulSoup(src, "lxml")
 
-    # получение кода товара
-    product_code = product.find(
-        "div", class_="n-catalog-item__photo-code"
-    ).find(
-        "span", class_="string bold n-catalog-item__click-copy n-catalog-item__ellipsis"
-    ).text
-
-    # получение названия бренда товара
-    product_brand = product.find(
-        "div", class_="n-catalog-item__brand d-none d-md-table-cell"
-    ).find(
-        class_="n-catalog-item__ellipsis"
-    ).text.strip()
-
-    # получение артикула товара
-    product_article = product.find(
-        "div", class_="n-catalog-item__article"
-    ).find(
-        "span", class_="n-catalog-item__ellipsis"
-    ).text
-
-    # получение названия товара
-    product_name = product.find(
-        "div", class_="string"
-    ).find(
-        "a", class_="n-catalog-item__name-link"
-    ).text.strip()
-
-    # получение цен товара
-    if product.find("div", class_="n-catalog-item__price-box col-12 col-md pr-0 mb-2") == None:
-        pass
-    else:
-
-        product_prices = product.find(
-            "div", class_="n-catalog-item__price-box col-12 col-md pr-0 mb-2"
-        ).find(
-            "ul"
-        ).find_all(
-            "li"
-        )
-
-        product_price_obj = {}
-
-        for price in product_prices:
-            type_price = price.find(
-                class_="fake mr-2 link-color price5").text.strip()
-            cur_price = price.find(class_="gray").text
-            product_price_obj[type_price] = cur_price
-
-    # получение количества товаров
-    count = product.find(
-        "span", class_="fake grass bold mr-0"
+    all_products = soup.find_all(
+        "div", class_="n-catalog-item relative grid-item n-catalog-item__product"
     )
 
-    if count != None:
-        product_count = product.find(
-            "div", class_="n-catalog-item__count-box"
+    for product in all_products:
+
+        # получение кода товара
+        product_code = product.find(
+            "div", class_="n-catalog-item__photo-code"
         ).find(
-            "span", class_="fake grass bold mr-0"
-        ).text.strip()[:-1]
-        product_count = int(product_count)
-    else:
-        count = product.find(
-            "span", class_="fake link-gray"
-        )
-        if count != None:
-            product_count = count.text
+            "span", class_="string bold n-catalog-item__click-copy n-catalog-item__ellipsis"
+        ).text
+
+        # получение названия бренда товара
+        product_brand = product.find(
+            "div", class_="n-catalog-item__brand d-none d-md-table-cell"
+        ).find(
+            class_="n-catalog-item__ellipsis"
+        ).text.strip()
+
+        # получение артикула товара
+        product_article = product.find(
+            "div", class_="n-catalog-item__article"
+        ).find(
+            "span", class_="n-catalog-item__ellipsis"
+        ).text
+
+        # получение названия товара
+        product_name = product.find(
+            "div", class_="string"
+        ).find(
+            "a", class_="n-catalog-item__name-link"
+        ).text.strip()
+
+        # получение цен товара
+        if product.find("div", class_="n-catalog-item__price-box col-12 col-md pr-0 mb-2") == None:
+            pass
         else:
-            product_count = product.find(
-                "offers"
-            ).get(":offers")
-            product_count = json.loads(product_count)[0]["quantity"]
 
-    result_mas.append(
-        {
-            "Код товара": product_code,
-            "Бренд товара": product_brand,
-            "Артикул товара": product_article,
-            "Цены товара": product_price_obj,
-            "Количество на складе": product_count,
-        }
-    )
+            product_prices = product.find(
+                "div", class_="n-catalog-item__price-box col-12 col-md pr-0 mb-2"
+            ).find(
+                "ul"
+            ).find_all(
+                "li"
+            )
+
+            product_price_obj = {}
+
+            for price in product_prices:
+                type_price = price.find(
+                    class_="fake mr-2 link-color price5").text.strip()
+                cur_price = price.find(class_="gray").text
+                product_price_obj[type_price] = cur_price
+
+        # получение количества товаров
+        count = product.find(
+            "span", class_="fake grass bold mr-0"
+        )
+
+        if count != None:
+            product_count = product.find(
+                "div", class_="n-catalog-item__count-box"
+            ).find(
+                "span", class_="fake grass bold mr-0"
+            ).text.strip()[:-1]
+            product_count = int(product_count)
+        else:
+            count = product.find(
+                "span", class_="fake link-gray"
+            )
+            if count != None:
+                product_count = count.text
+            else:
+                product_count = product.find(
+                    "offers"
+                ).get(":offers")
+                product_count = json.loads(product_count)[0]["quantity"]
+
+        result_mas.append(
+            {
+                "Код товара": product_code,
+                "Бренд товара": product_brand,
+                "Артикул товара": product_article,
+                "Цены товара": product_price_obj,
+                "Количество на складе": product_count,
+            }
+        )
+    
+    return result_mas
+
+
+result_object = {}
+
+count = 1
+
+for article in articles:
+
+	product_info = get_product_info(article)
+
+	result_object[count] = product_info
+
+	print(f"Complited {count}")
+
+	count += 1
+
+	sleep_time = uniform(5, 7)
+	sleep(sleep_time)
+
 
 with open("data.json", "w", encoding="utf-8") as file:
-    json.dump(result_mas, file, ensure_ascii=False)
+    json.dump(result_object, file, ensure_ascii=False)
--- parsingSites/autopiter_parser.py
+++ parsingSites/autopiter_parser.py
@@ -1,102 +1,177 @@
 import json
 import sys
 import requests
+from time import sleep
+from random import uniform
 from bs4 import BeautifulSoup
 
-article = "476Q1002901"
 
-url = f"https://autopiter.ru/goods/{article}"
+not_robot1 = "https://www.google.com/recaptcha/api2/reload"
+not_robot2 = "https://www.google.com/recaptcha/api2/clr"
+not_robot3 = "https://autopiter.ru/api/api/not-robot/verification"
+
+data = {"k": "6LeGDIoiAAAAAFLlm8kMJLRzbUJjCbhcW_GXqc5T"}
+
+recaptcha = {'recaptcha': "03AFcWeA59eqnHBi8p89tRn-DAcSQm0PYmJ_wODB4Ip67PF92FJcoqzVFxbt-4kSV5y0awFeOxzjJTdNp9-sfW1644mfQt2UPvPbfcJoWwlOdm8Ql6Irl1KXoKb5sRQJGzLn7x79qbJRzxfvgQQJHv9xU9_LSDons_WXtADsQCM-X3YU_hglmnvE56QpohkqcDdZx0nrmrzEj0b7TICTQ2i2PUdV5yqWul2TuaXpfklz5ctb7BfskNhPWqhufNOzFIzNP5BAjKkrnFGupaHzN7domuC_7BFEdboXYHDJbPoP3Ml3ZV2h3ZNHlfCgsOdW6Td6UtAisPSpB6XhTC4XF7CaKw9SOVX_9OgZwZIgJW-GJOAhnlYencbxlTXHEf_KHXLJ7yWJheAITTIW0-9MVLsJOvvBPJq5S1_nQab6M2khT1iBZTb3c5M5Z-SXnTiumzqcdhRoBTCY5hmqY2LDcLG_PPjLYxbjYoynZEaUK-g021o2v8f_p90L6j9i-MysnepGgnBlDKyVt0WV06UoLFbtfa1jfp5Cu45y5OHEnwgFoQsD9nFGqNyk3ymtGVNs6t9OMvFhIB88lUcLYjrvxYA0k281nhyCNmsU17qSjGNNtY76pmZ105cRo_wiICTsdYTSJH7CAPgmABkMIwjr6DmTs1NKOFc2T03L6NQ2SkEzZXHRvfW-bhjTstiCbsGG8wg00W88CLry0wE7F0O3WBJnq2iUTSF9wFG9NlY1CT-mRTROx5Dv9CWS7Pl01e0RQK9zNddkZDTUdKxrXwvC2_xfp7xPVsQeXjBTtRLHuxsXWvVie7-jifX3vTBraxW0sFdaGaJmKwQVIYXbM-k4ODdBPmt6nl0tE02Q"}
+
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+	"Set-Cookie": "guestId=XOuTXkQ01GTNSUl3c_p3f; Max-Age=2592000;"
+}
+
+req_not_robot1 = requests.post(not_robot1, data=json.dumps(data))
+req_not_robot2 = requests.post(not_robot2, data=json.dumps(data))
+req_not_robot3 = requests.post(not_robot3, data=json.dumps(recaptcha))
+
+print(req_not_robot1)
+print(req_not_robot2)
+print(req_not_robot3)
+
+proxies = [
+"http://qrX2br:0SrrNc@88.218.73.45:9860",
+"http://qrX2br:0SrrNc@88.218.75.218:9367",
+"http://qrX2br:0SrrNc@88.218.72.250:9833"
+]
+
+articles = ["322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010", 
+            "322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010",
+			"322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010",
+            "322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010",
+            "322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010",
+            "322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010",
+            "322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010",
+            "322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010",
+            "322403", "236-1028001", "5360-1118002", "238-1000001-03", "236-1106210-А2", "236-1306054-А", "238-1104001-10", "AVX13-1045LA", "100-3519050", "42021116010"]
+
+article = "322403"
+
 
 headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
 }
 
-req = requests.get(url, headers=headers)
 
-src = req.text
+def get_href_product(article):
 
-soup = BeautifulSoup(src, "lxml")
+	url = f"https://autopiter.ru/goods/{article}"
 
-product = soup.find("div", "IndividualTableRow__row___111l8")
+	req = requests.get(url, headers=headers)
 
-product_href = product.find(
-    "div", class_="IndividualTableRow__numberColumn___36MQf"
-).find("a", class_="IndividualTableRow__numberLink___1-eq1 common__link___1TmCU").get("href")
+	src = req.text
+	# print(src)
+	soup = BeautifulSoup(src, "lxml")
 
-# print(product_href)
+	product = soup.find("div", "IndividualTableRow__row___111l8")
 
-url_product = f"https://autopiter.ru{product_href}"
+	product_href = product.find(
+		"div", class_="IndividualTableRow__numberColumn___36MQf"
+	).find("a", class_="IndividualTableRow__numberLink___1-eq1 common__link___1TmCU").get("href")
 
-req_product = requests.get(url_product, headers=headers)
-
-src_product = req_product.text
-
-soup_product = BeautifulSoup(src_product, "lxml")
-
-products_information = soup_product.find(
-	"table", class_="NonRetailAppraiseTable__table___7gnpi"
-).find("tbody").find_all(
-	"tr", class_="NonRetailAppraiseTR__tr___3rq83"
-)
-
-full_product_info = []
-
-for product_info in products_information:
-	
-	# Получение региона продукта
-	product_region = product_info.find(
-		"div", class_="NonRetailAppraiseTR__regionInner___3FAjj"
-	).text
-
-	# Получение бренда продукта
-	product_brand = product_info.find(
-		"span", class_="NonRetailAppraiseTR__brandLink___v2r3Y ModalButton__root___pxLdy"
-	).find(
-		"span", class_="ModalButton__button___3fjTP new-common__unstyledButton___zigzA"
-	).text
-
-	# Получение артикула продукта
-	product_article = product_info.find_all("td")[2].text
-
-	# Получение имени продукта
-	product_name = product_info.find(
-		"td", class_="NonRetailAppraiseTR__nameCell___2k4_I"
-	).find("div", class_="NonRetailAppraiseTR__nameCellInner___sPiuJ").text
-
-	# Получение количетсва продукта
-	product_count = product_info.find(
-		"td", class_="NonRetailAppraiseTR__quantityCell___nzLgM"
-	).find(
-		"div", class_="NonRetailAppraiseTR__quantity___18gVh"
-	).text
-
-	# Получение информации через сколько придет продукт
-	product_time = product_info.find(
-		"td", class_="NonRetailAppraiseTR__daysCell___YR7jI"
-	).find("span", "NonRetailAppraiseTR__days___UfW_V").text
-
-	# Получение стоимости продукта
-	product_price = product_info.find(
-		"td", "NonRetailAppraiseTR__priceCell___2hvw7"
-	).find("div", "NonRetailAppraiseTR__priceWrapper___2sj3p").text
+	return product_href
 
 
-	full_product_info.append(
-        {
-            "Регион поставщика": product_region,
-            "Производитель": product_brand,
-            "Номер": product_article,
-            "Наименование": product_name,
-            "Наличие, шт": product_count,
-            "Срок (дн)": product_time,
-            "Цена": product_price,
-        }
-    )
-
-print(full_product_info)
+product_href = get_href_product(article)
 
 
-# print(soup_product.text)
+def get_product_info(href):
+
+	url_product = f"https://autopiter.ru{href}"
+
+	req_product = requests.get(url_product, headers=headers)
+
+	src_product = req_product.text
+
+	soup_product = BeautifulSoup(src_product, "lxml")
+
+	products_information = soup_product.find(
+		"table", class_="NonRetailAppraiseTable__table___7gnpi"
+	).find("tbody").find_all(
+		"tr", class_="NonRetailAppraiseTR__tr___3rq83"
+	)
+
+	full_product_info = []
+
+	for product_info in products_information:
+		
+		# Получение региона продукта
+		product_region = product_info.find(
+			"div", class_="NonRetailAppraiseTR__regionInner___3FAjj"
+		).text
+
+		# Получение бренда продукта
+		product_brand = product_info.find(
+			"span", class_="NonRetailAppraiseTR__brandLink___v2r3Y ModalButton__root___pxLdy"
+		).find(
+			"span", class_="ModalButton__button___3fjTP new-common__unstyledButton___zigzA"
+		).text
+
+		# Получение артикула продукта
+		product_article = product_info.find_all("td")[2].text
+
+		# Получение имени продукта
+		product_name = product_info.find(
+			"td", class_="NonRetailAppraiseTR__nameCell___2k4_I"
+		).find("div").text
+
+		# Получение количетсва продукта
+		product_count = product_info.find(
+			"td", class_="NonRetailAppraiseTR__quantityCell___nzLgM"
+		).find(
+			"div", class_="NonRetailAppraiseTR__quantity___18gVh"
+		).text
+
+		# Получение информации через сколько придет продукт
+		product_time = product_info.find(
+			"td", class_="NonRetailAppraiseTR__daysCell___YR7jI"
+		).find("span", "NonRetailAppraiseTR__days___UfW_V").text
+
+		# Получение стоимости продукта
+		product_price = product_info.find(
+			"td", "NonRetailAppraiseTR__priceCell___2hvw7"
+		).find("div", "NonRetailAppraiseTR__priceWrapper___2sj3p").text
+
+
+		full_product_info.append(
+			{
+				"Регион поставщика": product_region,
+				"Производитель": product_brand,
+				"Номер": product_article,
+				"Наименование": product_name,
+				"Наличие, шт": product_count,
+				"Срок (дн)": product_time,
+				"Цена": product_price,
+			}
+		)
+
+	return full_product_info
+
+
+result_object = {}
+
+count = 1
+
+for article in articles:
+
+
+	href_product = get_href_product(article=article)
+
+	# sleep_time = uniform(7, 10)
+	# sleep(sleep_time)
+
+	product_info = get_product_info(href_product)
+
+	result_object[article] = product_info
+
+	print(f"Complited {count}")
+
+	count += 1
+
+	sleep_time = uniform(20, 25)
+	sleep(sleep_time)
+
+
+
 with open("data.json", "w", encoding="utf-8") as file:
-    json.dump(full_product_info, file, ensure_ascii=False)
+    json.dump(result_object, file, ensure_ascii=False)
