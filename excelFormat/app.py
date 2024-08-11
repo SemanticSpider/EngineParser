0f8997c8bba70b31ed056600aafc889f0b2991d9
unknown <slava.gvozdev99@gmail.com>
2024-08-11 20:16:21 +0300

    Integration Timur Slava

--- excelFormat/app.py
+++ excelFormat/app.py
@@ -14,6 +14,12 @@ import pandas as pd
 import openpyxl as op
 from openpyxl import load_workbook
 import re
+import json
+import sys
+import requests
+from time import sleep
+from random import uniform
+from bs4 import BeautifulSoup
 
 class LoadCard(MDCard):
 
@@ -112,8 +118,17 @@ class EngineParser(MDApp):
     def launchParse(self, **kwargs):
         properties = {widget.name: widget for widget in self.screen.children if widget.name in ["LoadCard", "LoadDirectory"]}
         loadFile, savePath = [properties["LoadCard"].loadFile, properties["LoadDirectory"].savePath]
-        result = self.excelFormat(loadFile)
-        print(result)
+        goodsExcelInfo = self.excelFormat(loadFile)
+        goodsParseInfo = []
+        for goods in goodsExcelInfo["goods"]:
+            print(goods["mainArtikul"])
+            good = self.get_product_info(goods["mainArtikul"])
+            goodsParseInfo.append(good)
+            sleep_time = uniform(5, 7)
+            sleep(sleep_time)
+        with open("data.json", "w", encoding="utf-8") as file:
+            json.dump(goodsParseInfo, file, ensure_ascii=False)
+
     
     def excelFormat(self, excelFile):
         # Читаем с openpyxl
@@ -171,6 +186,114 @@ class EngineParser(MDApp):
                 })
             else: resultObject["errors"].append(artikul)
         return resultObject
+    
+    def get_product_info(self, article):
+
+        url = f"https://www.autoopt.ru/search/index?search={article}"
+        headers = {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+        }
+
+        req = requests.get(url, headers=headers)
+
+        src = req.text
+
+        result_mas = []
+
+        soup = BeautifulSoup(src, "lxml")
+
+        all_products = soup.find_all(
+            "div", class_="n-catalog-item relative grid-item n-catalog-item__product"
+        )
+
+        for product in all_products:
+
+            # получение кода товара
+            product_code = product.find(
+                "div", class_="n-catalog-item__photo-code"
+            ).find(
+                "span", class_="string bold n-catalog-item__click-copy n-catalog-item__ellipsis"
+            ).text
+
+            # получение названия бренда товара
+            product_brand = product.find(
+                "div", class_="n-catalog-item__brand d-none d-md-table-cell"
+            ).find(
+                class_="n-catalog-item__ellipsis"
+            ).text.strip()
+
+            # получение артикула товара
+            product_article = product.find(
+                "div", class_="n-catalog-item__article"
+            ).find(
+                "span", class_="n-catalog-item__ellipsis"
+            ).text
+
+            # получение названия товара
+            product_name = product.find(
+                "div", class_="string"
+            ).find(
+                "a", class_="n-catalog-item__name-link"
+            ).text.strip()
+            
+            product_price_obj = {}
+            # получение цен товара
+            if product.find("div", class_="n-catalog-item__price-box col-12 col-md pr-0 mb-2") == None:
+                pass
+            else:
+
+                product_prices = product.find(
+                    "div", class_="n-catalog-item__price-box col-12 col-md pr-0 mb-2"
+                ).find(
+                    "ul"
+                ).find_all(
+                    "li"
+                )
+
+
+
+                for price in product_prices:
+                    type_price = price.find(
+                        class_="fake mr-2 link-color price5").text.strip()
+                    cur_price = price.find(class_="gray").text
+                    product_price_obj[type_price] = cur_price
+
+            # получение количества товаров
+            count = product.find(
+                "span", class_="fake grass bold mr-0"
+            )
+
+            if count != None:
+                product_count = product.find(
+                    "div", class_="n-catalog-item__count-box"
+                ).find(
+                    "span", class_="fake grass bold mr-0"
+                ).text.strip()[:-1]
+                product_count = int(product_count)
+            else:
+                count = product.find(
+                    "span", class_="fake link-gray"
+                )
+                if count != None:
+                    product_count = count.text
+                else:
+                    product_count = product.find(
+                        "offers"
+                    ).get(":offers")
+                    product_count = json.loads(product_count)[0]["quantity"]
+
+            result_mas.append(
+                {
+                    "Код товара": product_code,
+                    "Бренд товара": product_brand,
+                    "Артикул товара": product_article,
+                    "Цены товара": product_price_obj,
+                    "Количество на складе": product_count,
+                }
+            )
+        
+        return result_mas
 
     
 
